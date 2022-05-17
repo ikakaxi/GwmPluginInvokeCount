a67b957f9e48b652927b74e6b8ff47ed5523b1e7
liuhc <i_kakaxi@163.com>
2022-05-17 19:21:53 +0800

    改为不需要上传到本地仓库的方式让app模块引入插件

--- gwmInvokeCountPlugin/src/main/groovy/com/gwm/plugin/GInfoPlugin.groovy
+++ gwmInvokeCountPlugin/src/main/groovy/com/gwm/plugin/GInfoPlugin.groovy
@@ -8,7 +8,10 @@ import org.gradle.api.Project
 public class GInfoPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        def android = project.extensions.getByType(AppExtension)
-        android.registerTransform(new InvokeCountTransform(project))
+        def android = project.extensions.findByType(AppExtension)
+        //插件为单独项目并且使用不需要本地仓库的方式时，需要对android判空，并且AppExtension也必须要用findByType查找
+        if (android != null) {
+            android.registerTransform(new InvokeCountTransform(project))
+        }
     }
 }
